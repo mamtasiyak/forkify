32596977dd9cb6b6e8bfb217b374452eaab12292
mamtasiyak <mamtasiyak111@gmail.com>
2022-03-20 21:00:57 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `5dd40a6b-95b4-48fb-8463-bc577803b27f`;
+export const MODAL_CLOSE_SEC = 2.5;
